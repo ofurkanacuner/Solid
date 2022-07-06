f25392d827bf6c60707bed7ca3246460d39e8f6a
Ö. Furkan Acuner <acuner.66.06@gmail.com>
2022-07-07 01:08:53 +0300

    Single Responsibility Principle

---
+++ SingleResponsibilityPrinciple/Iletisim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public  class IletisimGec
+    {
+        public bool EmailGonder(string Email, string Baslik, string Mesaj)
+        {
+            //Email gönderme işlemi başlaı
+
+            return true;
+        }
+
+        public bool TurkcelSMS(string TelNo, string Mesaj)
+        {
+            //Sms Gönderme işlemi başladı
+            return true;
+        }
+
+        public bool IletiMerkeziSms(string TelNo, string Mesaj)
+        {
+            //Sms Gönderme İşlemi yapıldı
+            return true;
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingleResponsibilityPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
